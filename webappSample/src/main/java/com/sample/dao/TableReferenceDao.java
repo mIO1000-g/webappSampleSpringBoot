f39e2f4b79b098d3c6583c08d6c35dca6f56565c
msend <msend@home2206>
2022-08-29 23:21:49 +0900

    Domaによるデータベースアクセス仮実装

--- webappSample/src/main/java/com/sample/dao/TableReferenceDao.java
+++ webappSample/src/main/java/com/sample/dao/TableReferenceDao.java
@@ -1,32 +1,40 @@
 package com.sample.dao;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.seasar.doma.Dao;
+import org.seasar.doma.Select;
+import org.seasar.doma.boot.ConfigAutowireable;
 
-public class TableReferenceDao {
+import com.sample.form.TableReferenceForm;
 
-	public List<HashMap<String, Object>> selectDetail() {
-		
-		List<HashMap<String, Object>> list = new ArrayList<>();
-		
-		HashMap<String, Object> map = new HashMap<>();
-		
-		map.put("no", 1);
-		map.put("employeeId", "1234567890");
-		map.put("name", "千田未央");
-		map.put("gender", "F");
-		map.put("genderName", "女");
-		map.put("birthday", "19890912");
-		map.put("enteringDate", "20140401");
-		map.put("retirementDate", "20230401");
-		map.put("departmentId", "1001");
-		map.put("departmentName", "開発");
-		map.put("updateDate", "20220826000000");
+@ConfigAutowireable
+@Dao
+public interface TableReferenceDao {
 
-		list.add(map);
-		
-		return list;
-	}
+//	public List<HashMap<String, Object>> selectDetail() {
+//		
+//		List<HashMap<String, Object>> list = new ArrayList<>();
+//		
+//		HashMap<String, Object> map = new HashMap<>();
+//		
+//		map.put("no", 1);
+//		map.put("employeeId", "1234567890");
+//		map.put("name", "千田未央");
+//		map.put("gender", "F");
+//		map.put("genderName", "女");
+//		map.put("birthday", "19890912");
+//		map.put("enteringDate", "20140401");
+//		map.put("retirementDate", "20230401");
+//		map.put("departmentId", "1001");
+//		map.put("departmentName", "開発");
+//		map.put("updateDate", "20220826000000");
+//
+//		list.add(map);
+//		
+//		
+
+	@Select
+	public List<Map<String, Object>> selectDetail(TableReferenceForm form);
 }
--- webappSample/src/main/java/com/sample/service/TableReferenceService.java
+++ webappSample/src/main/java/com/sample/service/TableReferenceService.java
@@ -1,9 +1,10 @@
 package com.sample.service;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sample.dao.TableReferenceDao;
@@ -13,29 +14,31 @@ import com.sample.form.TableReferenceRecord;
 @Service
 public class TableReferenceService {
 
-
-	public TableReferenceDao dao = new TableReferenceDao();
+	@Autowired
+	public TableReferenceDao dao;
 
 	public TableReferenceForm init(TableReferenceForm form) {
 
-		List<HashMap<String, Object>> detail = dao.selectDetail();
+		List<Map<String, Object>> detail = dao.selectDetail(form);
 
 		List<TableReferenceRecord> list = new ArrayList<>();
+		
+		int cnt = 1;
 
-		for (HashMap<String, Object> map : detail) {
+		for (Map<String, Object> map : detail) {
 
 			TableReferenceRecord rec = new TableReferenceRecord();
 
-			rec.setNo(map.get("no").toString());
-			rec.setEmployeeId(map.get("employeeId").toString());
-			rec.setName(map.get("name").toString());
-			rec.setGenderName(map.get("genderName").toString());
-			rec.setBirthday(map.get("birthday").toString());
-			rec.setEnteringDate(map.get("enteringDate").toString());
-			rec.setRetirementDate(map.get("retirementDate").toString());
-			rec.setDepartmentId(map.get("departmentId").toString());
-			rec.setDepartmentName(map.get("departmentName").toString());
-			rec.setUpdateDate(map.get("updateDate").toString());
+			rec.setNo(Integer.toString(cnt++));
+			rec.setEmployeeId(String.valueOf(map.get("employee_id")));
+			rec.setName(String.valueOf(map.get("name")));
+			rec.setGenderName(String.valueOf(map.get("gender_name")));
+			rec.setBirthday(String.valueOf(map.get("birthday")));
+			rec.setEnteringDate(String.valueOf(map.get("entering_date")));
+			rec.setRetirementDate(String.valueOf(map.get("retirement_date")));
+			rec.setDepartmentId(String.valueOf(map.get("department_id")));
+			rec.setDepartmentName(String.valueOf(map.get("department_name")));
+			rec.setUpdateDate(String.valueOf(map.get("update_date")));
 
 			list.add(rec);
 		}
